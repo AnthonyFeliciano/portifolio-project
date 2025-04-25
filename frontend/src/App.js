1bde8e93a83e1990628a7cf796a8a50a803320ea
AnthonyFeliciano <ttonyoliveira2@gmail.com>
2025-04-25 16:46:21 -0300

    feat(frontend) Ajuste dos tests do React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>Bem-vindo ao sistema de gerenciamento de usuários</h1>
         <h1>ESSE TITULO É APENAS PARA TESTE</h1>
       </header>
       <main>
